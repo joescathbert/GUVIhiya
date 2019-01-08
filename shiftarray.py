b9df8baeffe4aea3dba80d8adc7cfbce5454a8eb
Joes Cathbert <36276297+joescathbert@users.noreply.github.com>
2019-01-08 16:22:47 +0530

    Update shiftarray.py

--- shiftarray.py
+++ shiftarray.py
@@ -1,3 +1,6 @@
 n, k = map(int, input().split())
 arr = [int(x) for x in input().split()]
-print([arr[(i+(n-k))%n] for i in range(len(arr))])
+ar = [arr[(i+(n-k))%n] for i in range(len(arr))]
+for i in ar:
+    print(i,end=" ")
+
