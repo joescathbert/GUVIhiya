ec273464f93aacf898f9116e8006b6f8cbc629c1
Joes Cathbert <36276297+joescathbert@users.noreply.github.com>
2019-01-09 23:50:10 +0530

    Update firstrepeat.py

--- firstrepeat.py
+++ firstrepeat.py
@@ -1,13 +1,11 @@
 n = int(input())
 arr = [int(x) for x in input().split()]
-dic = {}
-for a in range(len(arr)):
-    if arr[a] not in dic:
-        dic[arr[a]] = [1,a]
+mem = []
+for a in arr:
+    if a not in mem:
+        mem.append(a)
     else:
-        dic[arr[a]][0] += 1
-arr = [(i, j[1]) for i,j in dic.items() if j[0] > 1]
-if len(arr) == 0:
-    print("unique")
+        print(a)
+        break
 else:
-    print(min(arr, key=lambda x:x[1])[0])
+    print("unique")
