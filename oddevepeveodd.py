80ac6b39b70e0989b69b696839988cc6b8ce9a9a
Joes Cathbert <36276297+joescathbert@users.noreply.github.com>
2019-01-10 00:15:35 +0530

    Update oddevepeveodd.py

--- oddevepeveodd.py
+++ oddevepeveodd.py
@@ -1,5 +1,7 @@
 n = int(input())
 arr = [int(x) for x in input().split()]
+a = []
 for i in range(len(arr)):
     if (i % 2 == 0 and arr[i] % 2 == 1) or (i % 2 == 1 and arr[i] % 2 == 0):
-        print(arr[i],end=' ')
+       a.append(str(arr[i]))
+print(" ".join(a))
