dd7a6dcc2340ea4e6238846df91bd50b71352f1b
Joes Cathbert <36276297+joescathbert@users.noreply.github.com>
2019-01-09 23:03:55 +0530

    Update reponceintwice.py

--- reponceintwice.py
+++ reponceintwice.py
@@ -1,1 +1,10 @@
-
+num = int(input())
+a = input().split()
+diction = {}
+for k in a:
+	if k not in diction:
+		diction[k] = 1
+	else:
+		diction[k] += 1
+a = [(x,y) for x,y in diction.items()]
+print(int(min(a, key=lambda x:x[1])[0]))
