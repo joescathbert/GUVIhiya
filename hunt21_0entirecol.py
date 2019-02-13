6bc044b09673f0ee12ff8fe08a16990dc6d526db
Joes Cathbert <36276297+joescathbert@users.noreply.github.com>
2019-02-13 16:25:12 +0530

    Update hunt21_0entirecol.py

--- hunt21_0entirecol.py
+++ hunt21_0entirecol.py
@@ -1,8 +1,14 @@
 m, n = map(int, input().split())
 arr = []
+r = []
+c = []
 for x in range(m):
   arr.append([v for v in input().split()][:n])
 for i in range(m):
-  if '0' in arr[i]:
-    arr[i] = ['0']*n
+  for j in range(n):
+    if arr[i][j] == '0':
+      if i not in r: r.append(i)
+      if j not in c: c.append(i)
+arr = [arr[i][j] if i not in r and j not in c else '0' for i in range(m) for j in range(n)]
+for i in range(m):  
   print(" ".join(arr[i]))
